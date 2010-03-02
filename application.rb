ad050ae734ba3c1198f2fde5421d7e69889a0b13
Christoph Helma <helma@in-silico.de>
2010-03-02 11:18:39 +0100

    version bumped to 1.3.1

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-gem 'opentox-ruby-api-wrapper', '= 1.3.0'
+gem 'opentox-ruby-api-wrapper', '= 1.3.1'
 require 'opentox-ruby-api-wrapper'
 
 LOGGER.progname = File.expand_path(__FILE__)
